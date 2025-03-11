b849e48874d2547876bc76de285866d8eccd0f28
Schousboe <jakobschousboe@hotmail.com>
2025-03-11 01:01:59 +0100

    JS-JS.js

--- JS-JS.js
+++ JS-JS.js
@@ -3,12 +3,12 @@ var box = document.getElementById('farve-box');
 
 button.onclick = function changeColor(){
     if(box.style.background == 'red'){
-     box.style.background = 'blue'
+        box.style.background = 'blue';
     }else if(box.style.background == 'blue'){
-     box.style.background = 'green'
+        box.style.background = 'green';
     }else if(box.style.background == 'green'){
-     box.style.background = 'black'
-    }else (box.style.background == 'black'){
-     box.style.background = 'red'
+        box.style.background = 'black';
+    }else {
+        box.style.background = 'red';
     }
 }
